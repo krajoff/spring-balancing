b5a7df63543c1981c0cbf975e4625e9fcff27b70
krajoff <t0k@bk.ru>
2024-03-19 10:47:35 +0300

    Add direcoty auth. Add error handling when user already exsist

--- src/main/java/com/example/balancing/controllers/web/WebRegistrationController.java
+++ src/main/java/com/example/balancing/controllers/web/WebRegistrationController.java
@@ -3,11 +3,15 @@ package com.example.balancing.controllers.web;
 import com.example.balancing.models.user.User;
 import com.example.balancing.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping
@@ -15,26 +19,29 @@ public class WebRegistrationController {
     @Autowired
     private UserService userService;
 
+    private Boolean isExist = false;
+
     @GetMapping("/register")
-    public String showRegisterForm() {
-        return "register";
+    public String showRegisterForm(Model model) {
+        model.addAttribute("isExist", isExist);
+        return "auth/register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@RequestParam(required = true, defaultValue = "!@") String username,
-                               @RequestParam(required = true, defaultValue = "!@") String password) {
-        if (!(username.equals("!@") & password.equals("!@"))) {
-            System.out.println(username + " " + password);
+    public String registerUser(@RequestParam String username, @RequestParam String password) {
+        if (userService.getUserByUsername(username) == null) {
             User user = new User();
             user.setUsername(username);
             user.setPassword(password);
             userService.saveUser(user);
+            return "redirect:/login";
         }
-        return "redirect:/unit/index";
+        isExist = true;
+        return "redirect:/register";
     }
 
     @GetMapping("/login")
     String login() {
-        return "login";
+        return "auth/login";
     }
 }
